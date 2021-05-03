b66f7f7b0e163f7d4cc06751526387340a517c55
Carlos Eduardo <carlos.lima@sanp.com.br>
2021-05-03 07:43:31 -0300

    Add mongodb and env variables

---
+++ README.md
@@ -0,0 +1,2 @@
+# qualicorp-dev-test
+Aplicação de CRUD de usuários para vaga de desenvolvedor na Qualicorp
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
 const Routes = require('./main/config/routes-map.js')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+dotenv.config()
 app.use(cors())
 app.use(bodyParser.json())
 Routes(app)
 app.use(express.urlencoded({ extended: false }))
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const PORT = process.env.PORT || 3001
 
